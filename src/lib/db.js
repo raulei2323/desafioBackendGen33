caecf7afb6f0bff787fdbcdcaead2c73c4a19220
Raul Elizalde <raulei2323@gmail.com>
2024-06-09 01:44:51 -0600

    first commit added db.js and server.js

---
+++ src/lib/db.js
@@ -0,0 +1,11 @@
+const mongoose = require ("mongoose")
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+
+const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+
+function connect () {
+    return mongoose.connect(URI)
+}
+
+module.exports = { connect }
